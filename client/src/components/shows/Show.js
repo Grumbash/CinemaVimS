8db9957f3ae408149736213e67f76d3f987dcbd7
Rossev Gleb <rossevgleb@gmail.com>
2018-08-17 15:16:13 +0300

    in dev

--- client/src/components/shows/Show.js
+++ client/src/components/shows/Show.js
@@ -1,8 +1,9 @@
 import React from "react";
 import moment from "moment";
+import { Link } from "react-router-dom";
 export default function Show(props) {
   const {
-    id,
+    _id,
     date,
     hallId: {
       No,
@@ -10,37 +11,41 @@ export default function Show(props) {
     },
     movieId: { title, duration }
   } = props;
+  const id = _id;
+
   return (
-    <div attr-id={id}>
-      <h2>
-        Film: {title}{" "}
-        <p>
-          {" "}
-          Duration:{" "}
-          {moment()
-            .startOf("day")
-            .seconds(+duration)
-            .format("H:mm")}
-        </p>
-      </h2>
-      <p>City: {city}</p>
-      <div>
-        <p>Theater: {name}</p>
-        <p>Hall: {No}</p>
-        <p>
-          Date:{" "}
-          {moment(date)
-            .utc()
-            .format("DD-MM-YYYY")}
-        </p>
-        <p>
-          Time:{" "}
-          {moment(date)
-            .utc()
-            .format("HH:mm")}
-        </p>
+    <Link to={{ pathname: `/shows/${id}`, state: { id: id } }}>
+      <div attr-id={id}>
+        <h2>
+          Film: {title}{" "}
+          <p>
+            {" "}
+            Duration:{" "}
+            {moment()
+              .startOf("day")
+              .seconds(+duration)
+              .format("H:mm")}
+          </p>
+        </h2>
+        <p>City: {city}</p>
+        <div>
+          <p>Theater: {name}</p>
+          <p>Hall: {No}</p>
+          <p>
+            Date:{" "}
+            {moment(date)
+              .utc()
+              .format("DD-MM-YYYY")}
+          </p>
+          <p>
+            Time:{" "}
+            {moment(date)
+              .utc()
+              .format("HH:mm")}
+          </p>
+        </div>
+        <hr />
       </div>
-      <hr />
-    </div>
+    </Link>
   );
 }
